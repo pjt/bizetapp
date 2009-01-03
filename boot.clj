4535aa07acd7c6934e05e3f5841bd8637e298476
trolard <trolard@a789088f-44d7-48a5-acf8-fa2aea575569>
2009-01-03 00:59:03 +0000

    More changes to work w/ compojure.jar...
    git-svn-id: http://svn.hdwdev.artsci.wustl.edu/dev/trunk/bizetapp@121 a789088f-44d7-48a5-acf8-fa2aea575569

--- boot.clj
+++ boot.clj
@@ -1,17 +1,9 @@
-;; This file will be evaluated when script/repl or script/run are executed.
-;; Populate this file with the code required to start your application.
-
-;; An example boot setup is included below:
 (use 'compojure.jetty)
 
-;; Load app/... code
 (require 'bizet)
 
-;; Define a new HTTP server on port 8080, with the hello-world servlet
-;; defined in app/example.clj
 (defserver server
   {:port 8080}
-  ;"/*" example/hello-world)
   "/*" bizet/bizetapp)
 
 ;; Start the server
